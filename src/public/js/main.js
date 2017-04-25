5cf9a429301c0ea53f78d02444a307d7ef5c4a33
Chris Hager <chris@linuxuser.at>
2017-04-25 12:49:38 +0200

    cleanup

--- src/public/js/main.js
+++
@@ -1,1 +0,0 @@
-function followOnTwitter(){var t=550,e=420,o=screen.height,n=screen.width;left=Math.round(n/2-t/2),top=0,o>e&&(top=Math.round(o/2-e/2)),window.open("https://twitter.com/intent/follow?screen_name=ghoustgame","intent","scrollbars=yes,resizable=yes,toolbar=no,location=yes,width="+t+",height="+e+",left="+left+",top="+top)}
